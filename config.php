c4d8c50b57d63ef334eca0b5b51fe6745bb8cc45
orionblac <73790560+orionblac@users.noreply.github.com>
2020-11-03 17:22:25 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+//xxx Wajib Diisi
+
+$token = "N2M4OGY1NGYtMTdlZS00NDI5LTk3NDMtOGNjMjNlZDNiNmQx";
+$uuid = "02f85f8a-317e-4486-8706-ffacae1e7b66";
+$userid = "23818850";
+
+$eksternal_version = "2.0.1";
+$version = "42";
+$device = "9";
+$timezone = "7";
+$lang = "id";
+
+$spin_version = "40";
+$user_agent = "Mozilla/5.0 (Linux; U; Android 8.0.0; en-US; AUM-AL20 Build/HONORAUM-AL20) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.108 UCBrowser/13.3.0.1302 Mobile Safari/537.36"; //Search in Google Chrome With Keyword "my user agent"
